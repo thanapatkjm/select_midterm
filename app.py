62617eabfea0043656e32b5c28cd42c9223c2bb8
Dxcuer <thanapatkjm@hotmail.com>
2018-10-25 13:54:05 +0700

    Create Method airData

--- app.py
+++ app.py
@@ -3,21 +3,37 @@ from spyne.server.wsgi import WsgiApplication
 from spyne import Iterable, Integer, Unicode, srpc, Application, String
 from spyne.service import ServiceBase
 from spyne.protocol.soap import Soap11
+from spyne.protocol.http import HttpRpc
+from lxml import etree
+import dicttoxml
 
 from flask import Flask
 
 app = Flask(__name__)
 
-class studentInformation(ServiceBase):
+class Data(ServiceBase):
     
-
     @srpc(_returns=Iterable(String))
-    def CCTVList():
-        yield 'CCTV01'
-        yield 'CCTV02'
-        yield 'CCTV03'
+    def airData():
+        airInfo = []
+        listAir_no = ['1', '2','3','4','5']
+        listAir_date = ['11/12/18','11/12/18','11/12/18','11/12/18','11/12/18',]
+        listAir_temp = ['32','30','34','35','36']
+        listAir_humid = ['12','13','14','15','16']
+        for i in listAir_no:
+            tempData['No'] = listAir_no[i]
+            tempData['Date'] = listAir_date[i]
+            tempData['Temp'] = listAir_temp[i]
+            tempData['Humid'] = listAir_humid[i]
+            airInfo.append(tempData)
+        airXml = dicttoxml.dicttoxml(airInfo)
+            
+    
+##    @srpc(_returns=Iterable(String))
+##    def myData():
+####        myTempData['Name']
 
-application = Application([studentInformation],
+application = Application([Data],
     tns='spyne.examples.cctv',
     in_protocol=Soap11(validator='lxml'),
     out_protocol=Soap11()
